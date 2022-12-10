be6027c87f5a198ee3cbbe05742aa32d243c745c
Vadim Hladchenko <vadimhl@gmail.com>
2022-12-10 22:04:10 +0200

    razbiv

--- task.py
+++ task.py
@@ -1,3 +1,5 @@
+#import math
+
 # 2. Напишите template строки, который можно будет многократно переиспользовать, 
 # вставляя в него имя и фамилию человека. Используйте метод строки "format".
 #  
@@ -35,3 +37,48 @@ def upper_price2( list_price ):
 sample_list = [ 'qwerty', 'first price', 'zxcvb', 'second price', 'price again' ]
 print(upper_price(sample_list))
 print(upper_price2(sample_list))
+ 
+# 5. Напишите функцию, которая принимает список, и число. 
+#    Функция должна разбить список на N кусков, 
+#    переданных в функцию в качестве втрого аргумента. 
+#    Выполнить проверки по здравому смыслу (например, 
+#    нет смысла пытаться разбить список из 3 элементов на 4 элемента)
+
+def razbiv( lst, n):
+    length = len(lst)
+    if ( length >= n):
+        rest = length // n 
+        mod = length % n
+        res = []
+        #print(f'{length=} {n=} {rest=} {mod=}')
+        start = 0
+        while start < length:
+            if mod > 0:
+                cnt = rest+1
+                mod -= 1
+            else:
+                cnt = rest
+            res.append(lst[start:start+cnt])
+            start += cnt
+        return res
+    else:
+        return lst
+
+def razbiv2( lst, n):
+    length = len(lst)
+    if ( length >= n):
+        rest = length // n 
+        mod = length % n
+        start = 0
+        #print(f'{length=} {n=} {rest=} {mod=}')
+        res = [lst[start:(start:=start+rest+(1 if (mod:=mod-1) >=0 else 0))] for i in range(0,n)] 
+            #+ [lst[start:(start:=start+rest)] for i in range(mod, n)]
+        return res 
+        # 1 4 7 9 
+        # 2 5 8 0 
+        # 3 6
+
+lst = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango", "pear", "plum"]  
+for i in range(2, len(lst)+1):
+    res = razbiv(lst, i)
+    print(f'{i} -> {len(res)} {res}')      
