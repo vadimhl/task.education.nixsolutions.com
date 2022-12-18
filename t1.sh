c0c995d303c81bfb324c147adca01eb09f5d72f8
Vadim Hladchenko <vadimhl@gmail.com>
2022-12-18 11:51:18 +0200

    test

---
+++ t1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# for i in {1..254}
+# do
+#   (ping -c2 -w 2 192.168.1.$i >/dev/null 2>&1 && echo 192.168.1.$i) &
+# done
+# wait
+ps axo rss,comm,pid | awk '{ proc_list[2] += $1; } END { for (proc in proc_list) { printf(1/=1024;printf "%.0fMB\t",$1}{print $2}'
---
+++ test.py
@@ -0,0 +1,21 @@
+# print('👀🔥'.encode(encoding="ascii", errors="namereplace"))
+# print('✌️‼'.encode(encoding="ascii", errors="namereplace"))
+# print('💙💛'.encode(encoding="ascii", errors="namereplace"))
+# print('🇺🇦'.encode(encoding="ascii", errors="namereplace"))
+# print('\N{BLUE HEART}\N{YELLOW HEART}')
+# print('\U0001F3F4\N{REGIONAL INDICATOR SYMBOL LETTER U}'
+#       + '\N{REGIONAL INDICATOR SYMBOL LETTER A}\U000E007F')
+# print('🇺🇦'.encode("utf-8"))
+# ua1 = '\U0001F1FA\U0001F1E6'
+# ua2 = '🇺🇦\n'
+# print(ua1 == ua2)
+# print('\U0001F1FA\U0001F1E6')
+# "\u{0001F1FA}\u{0001F1E6}"
+# '\\uD83C\\uDDFA\\uD83C\\uDDE6'
+
+# from typing import Optional
+
+
+# print(my_decorated_function('Hello, World!'))
+with open('ttt.txt', 'w') as f:
+    f.write('🇺🇦💙💛✌️‼👀🔥')
